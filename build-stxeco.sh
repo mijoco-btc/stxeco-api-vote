118d214286722dd1e21f3def7d3809b172ebad4b
mike <mjoecohen@gmail.com>
2024-10-04 12:53:33 +0100

    changed pox3 to pox to make general. added a build file for remote ubuntu docker build

--- build-stxeco.sh
+++ build-stxeco.sh
@@ -17,16 +17,17 @@ printf "====================================================\n"
 printf "Building on: $SERVER as docker container: $DOCKER_NAME \n"
 
 source ~/.profile;
+#cd ~/hubgit/stxeco/stxeco-api-vote
+#git pull https://github.com/radicleart/stxeco-api-vote.git daoless
 docker login;
 docker build -t $DOCKER_NAME .
 docker tag mijoco/stxeco_api_vote mijoco/stxeco_api_vote
 docker push mijoco/stxeco_api_vote:latest
-docker run -d -t -i --network host --env-file ~/.env --name $DOCKER_NAME -p 6060:6060 $DOCKER_NAME
-docker logs -f $DOCKER_NAME
 
 #docker pull mijoco/stxeco_api_vote;
-#docker rm -f stxeco_api_vote  
-#docker run -d -t -i --network host --env-file ~/.env --name stxeco_api_vote -p 3010:3010 mijoco/stxeco_api_vote
+docker rm -f stxeco_api_vote  
+docker run -d -t -i --network host --env-file ~/.env --name $DOCKER_NAME -p 6060:6060 $DOCKER_NAME
+docker logs -f $DOCKER_NAME
 
 printf "Finished....\n"
 printf "====================================================\n\n"
