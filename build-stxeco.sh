fffc9680234ec4c056e60ccb6638412478cc5bf9
mike <mjoecohen@gmail.com>
2025-06-23 22:48:59 +0100

    debugging a cahing problem in docker build

--- build-stxeco.sh
+++ build-stxeco.sh
@@ -23,11 +23,12 @@ docker login;
 #docker build -t mijoco/stxeco_api_vote .
 docker build --no-cache -t mijoco/stxeco_api_vote .
 docker tag mijoco/stxeco_api_vote mijoco/stxeco_api_vote
+docker tag mijoco/stxeco_api_vote stxeco_api_vote
 docker push mijoco/stxeco_api_vote:latest
 
 #docker pull mijoco/stxeco_api_vote;
 docker rm -f stxeco_api_vote  
-docker run -d -t -i --network host --env-file ~/.env --name $DOCKER_NAME -p 6060:6060 $DOCKER_NAME
+docker run -d -t -i --network host --env-file ~/.env --name $DOCKER_NAME -p 6060:6060 mijoco/stxeco_api_vote:latest
 docker logs -f $DOCKER_NAME
 
 printf "Finished....\n"
