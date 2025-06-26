b3346e590f99c106378389f31cb1c9ae738637c5
mike <mjoecohen@gmail.com>
2025-06-26 07:40:42 +0100

    Fix for Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client

--- src/routes/proposals/proposalRoutes.ts
+++ src/routes/proposals/proposalRoutes.ts
@@ -1,33 +1,9 @@
 import express from "express";
-import {
-  FundingData,
-  TentativeProposal,
-  VotingEventProposeProposal,
-  lookupContract,
-} from "@mijoco/stx_helpers/dist/index";
+import { FundingData, TentativeProposal, VotingEventProposeProposal, lookupContract } from "@mijoco/stx_helpers/dist/index";
 import { fetchByBaseDaoEvent } from "../../lib/events/event_helper_base_dao";
-import {
-  fetchActiveProposeEvents,
-  fetchAllConcludedEvents,
-  fetchAllProposeEvents,
-  fetchLatestProposal,
-  fetchProposeEvent,
-  toggleSipStatus,
-  updateStackerData,
-} from "../../lib/events/event_helper_voting_contract";
-import {
-  generateAddresses,
-  generateAddressesV2,
-  generateBitcoinAddressV2,
-  generateStacksAddressV2,
-  getFunding,
-  getMetaData,
-} from "../../lib/events/proposal";
-import {
-  fetchTentativeProposals,
-  findTentativeProposalByContractId,
-  saveOrUpdateTentativeProposal,
-} from "../../lib/data/db_models";
+import { fetchActiveProposeEvents, fetchAllConcludedEvents, fetchAllProposeEvents, fetchLatestProposal, fetchProposeEvent, toggleSipStatus, updateStackerData } from "../../lib/events/event_helper_voting_contract";
+import { generateAddresses, generateAddressesV2, generateBitcoinAddressV2, generateStacksAddressV2, getFunding, getMetaData } from "../../lib/events/proposal";
+import { fetchTentativeProposals, findTentativeProposalByContractId, saveOrUpdateTentativeProposal } from "../../lib/data/db_models";
 import { getConfig } from "../../lib/config";
 
 const router = express.Router();
@@ -62,21 +38,15 @@ router.get("/generate-addresses/:msg", async (req, res, next) => {
   }
 });
 
-router.get(
-  "/generate-stacker-data/:sip/:proposalId",
-  async (req, res, next) => {
-    try {
-      const proposal = await updateStackerData(
-        Boolean(req.params.sip),
-        req.params.proposalId
-      );
-      return res.send(proposal);
-    } catch (error) {
-      console.log("Error in routes: ", error);
-      next("An error occurred fetching pox-info.");
-    }
+router.get("/generate-stacker-data/:sip/:proposalId", async (req, res, next) => {
+  try {
+    const proposal = await updateStackerData(Boolean(req.params.sip), req.params.proposalId);
+    return res.send(proposal);
+  } catch (error) {
+    console.log("Error in routes: ", error);
+    next("An error occurred fetching pox-info.");
   }
-);
+});
 
 router.get("/toggle-sip-status/:proposalId", async (req, res, next) => {
   try {
@@ -90,10 +60,7 @@ router.get("/toggle-sip-status/:proposalId", async (req, res, next) => {
 
 router.get("/get-executed-proposals/:daoContract", async (req, res, next) => {
   try {
-    const extensions = await fetchByBaseDaoEvent(
-      req.params.daoContract,
-      "execute"
-    );
+    const extensions = await fetchByBaseDaoEvent(req.params.daoContract, "execute");
     return res.send(extensions);
   } catch (error) {
     console.log("Error in routes: ", error);
@@ -103,31 +70,25 @@ router.get("/get-executed-proposals/:daoContract", async (req, res, next) => {
 
 router.get("/get-proposal/:proposalContractId", async (req, res, next) => {
   try {
-    const proposal: VotingEventProposeProposal = await fetchLatestProposal(
-      req.params.proposalContractId
-    );
+    const proposal: VotingEventProposeProposal = await fetchLatestProposal(req.params.proposalContractId);
     if (!proposal) res.sendStatus(404);
     return res.send(proposal);
   } catch (error) {
-    res.sendStatus(404);
+    console.log("Error in routes: ", error);
+    next("An error occurred fetching pox-info.");
   }
 });
 
-router.get(
-  "/get-funding/:submissionContract/:proposalContract",
-  async (req, res, next) => {
-    try {
-      const funding: FundingData = await getFunding(
-        req.params.submissionContract,
-        req.params.proposalContract
-      );
-      if (!funding) res.sendStatus(404);
-      return res.send(funding);
-    } catch (error) {
-      res.sendStatus(404);
-    }
+router.get("/get-funding/:submissionContract/:proposalContract", async (req, res, next) => {
+  try {
+    const funding: FundingData = await getFunding(req.params.submissionContract, req.params.proposalContract);
+    if (!funding) res.sendStatus(404);
+    return res.send(funding);
+  } catch (error) {
+    console.log("Error in routes: ", error);
+    next("An error occurred fetching pox-info.");
   }
-);
+});
 
 router.get("/all-proposals", async (req, res, next) => {
   try {
@@ -155,47 +116,40 @@ router.get("/tentative-proposals", async (req, res, next) => {
 
 router.get("/get-tentative-proposal/:proposalId", async (req, res, next) => {
   try {
-    const proposal = await findTentativeProposalByContractId(
-      req.params.proposalId
-    );
+    const proposal = await findTentativeProposalByContractId(req.params.proposalId);
     return res.send(proposal);
   } catch (error) {
     return res.send([]);
   }
 });
 
-router.get(
-  "/create-tentative-proposal/:start/:end/:proposalId",
-  async (req, res, next) => {
-    try {
-      const deployed = await lookupContract(
-        getConfig().stacksApi,
-        req.params.proposalId
-      );
-      console.log("deployed: ", deployed);
-      if (!deployed) res.sendStatus(404);
-      const tentativeproposal = {
-        tag: req.params.proposalId,
-        visible: true,
-        proposalMeta: getMetaData(deployed.source_code),
-        expectedStart: Number(req.params.start),
-        expectedEnd: Number(req.params.end),
-        stacksDeployHeight: deployed.block_height,
-        info: undefined,
-        submissionData: {
-          contractId: req.params.proposalId,
-          transaction: deployed.tx_id,
-        },
-        proposer: undefined,
-        votingContract: undefined,
-      } as TentativeProposal;
-      await saveOrUpdateTentativeProposal(tentativeproposal);
-      return res.send(tentativeproposal);
-    } catch (error) {
-      return res.send([]);
-    }
+router.get("/create-tentative-proposal/:start/:end/:proposalId", async (req, res, next) => {
+  try {
+    const deployed = await lookupContract(getConfig().stacksApi, req.params.proposalId);
+    console.log("deployed: ", deployed);
+    if (!deployed) res.sendStatus(404);
+    const tentativeproposal = {
+      tag: req.params.proposalId,
+      visible: true,
+      proposalMeta: getMetaData(deployed.source_code),
+      expectedStart: Number(req.params.start),
+      expectedEnd: Number(req.params.end),
+      stacksDeployHeight: deployed.block_height,
+      info: undefined,
+      submissionData: {
+        contractId: req.params.proposalId,
+        transaction: deployed.tx_id,
+      },
+      proposer: undefined,
+      votingContract: undefined,
+    } as TentativeProposal;
+    await saveOrUpdateTentativeProposal(tentativeproposal);
+    return res.send(tentativeproposal);
+  } catch (error) {
+    console.log("Error in routes: ", error);
+    next("An error occurred fetching pox-info.");
   }
-);
+});
 
 router.get("/active-proposals", async (req, res, next) => {
   try {
